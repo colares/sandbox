d747258cca8f5e266241d7f0f0414c7e67943961
Thiago Colares <thiago@apimenti.com.br>
2013-07-25 14:29:36 -0300

    Hello World

---
+++ hw.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * just to say hello
+ */
+class HelloWorld {
+
+	/**
+	 * I just call to say hello world :P
+	 */
+	static public function say() {
+		return 'Hello World!';
+	}
+}
+
+print HelloWorld::say();
+
+?>
